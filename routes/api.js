c2e99860ebe07afbe085fb1230d34db03d04cb8e
silvig <silvigonzalez92@gmail.com>
2025-06-24 10:14:39 -0300

    proyecto rastreador de problemas

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,104 @@
 'use strict';
 
-module.exports = function (app) {
+const { MongoClient, ObjectId } = require('mongodb');
+const CONNECTION = process.env.DB;
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+module.exports = function (app) {
+  MongoClient.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(client => {
+      const db = client.db('issuetracker');
+      const collection = db.collection('issues');
+
+      app.route('/api/issues/:project')
+
+        // GET: Ver problemas
+        .get(async (req, res) => {
+          const filter = { ...req.query };
+
+          if (filter._id) filter._id = new ObjectId(filter._id);
+          if (filter.open) filter.open = filter.open === 'true';
+
+          const issues = await collection.find(filter).toArray();
+          res.json(issues);
+        })
+
+        // POST: Crear problema
+        .post(async (req, res) => {
+          const { issue_title, issue_text, created_by, assigned_to = '', status_text = '' } = req.body;
+
+          if (!issue_title || !issue_text || !created_by) {
+            return res.json({ error: 'required field(s) missing' });
+          }
+
+          const newIssue = {
+            issue_title,
+            issue_text,
+            created_by,
+            assigned_to,
+            status_text,
+            created_on: new Date(),
+            updated_on: new Date(),
+            open: true
+          };
+
+          const result = await collection.insertOne(newIssue);
+          res.json({
+            _id: result.insertedId,
+            ...newIssue
+          });
+        })
+
+        // PUT: Actualizar problema
+        .put(async (req, res) => {
+          const { _id, ...fields } = req.body;
+
+          if (!_id) return res.json({ error: 'missing _id' });
+
+          const updateFields = {};
+          for (let key in fields) {
+            if (fields[key]) {
+              updateFields[key] = fields[key];
+            }
+          }
+
+          if (Object.keys(updateFields).length === 0) {
+            return res.json({ error: 'no update field(s) sent', _id });
+          }
+
+          updateFields.updated_on = new Date();
+
+          try {
+            const result = await collection.updateOne(
+              { _id: new ObjectId(_id) },
+              { $set: updateFields }
+            );
+
+            if (result.modifiedCount === 0) {
+              return res.json({ error: 'could not update', _id });
+            }
+
+            res.json({ result: 'successfully updated', _id });
+          } catch (err) {
+            res.json({ error: 'could not update', _id });
+          }
+        })
+
+        // DELETE: Eliminar problema
+        .delete(async (req, res) => {
+          const { _id } = req.body;
+
+          if (!_id) return res.json({ error: 'missing _id' });
+
+          try {
+            const result = await collection.deleteOne({ _id: new ObjectId(_id) });
+            if (result.deletedCount === 0) {
+              return res.json({ error: 'could not delete', _id });
+            }
+            res.json({ result: 'successfully deleted', _id });
+          } catch (err) {
+            res.json({ error: 'could not delete', _id });
+          }
+        });
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+    .catch(err => console.error('MongoDB connection error:', err));
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,156 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+let testId;
+
+suite('Functional Tests', function () {
+  test('Create issue with all fields', function (done) {
+    chai.request(server)
+      .post('/api/issues/test')
+      .send({
+        issue_title: 'Issue 1',
+        issue_text: 'Text',
+        created_by: 'User',
+        assigned_to: 'Dev',
+        status_text: 'In QA'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.issue_title, 'Issue 1');
+        testId = res.body._id;
+        done();
+      });
+  });
+
+  test('Create issue with required fields', function (done) {
+    chai.request(server)
+      .post('/api/issues/test')
+      .send({
+        issue_title: 'Issue 2',
+        issue_text: 'Text',
+        created_by: 'User'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.issue_title, 'Issue 2');
+        done();
+      });
+  });
+
+  test('Create issue missing required fields', function (done) {
+    chai.request(server)
+      .post('/api/issues/test')
+      .send({ issue_title: '', issue_text: '', created_by: '' })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'required field(s) missing');
+        done();
+      });
+  });
+
+  test('View issues on project', function (done) {
+    chai.request(server)
+      .get('/api/issues/test')
+      .end((err, res) => {
+        assert.isArray(res.body);
+        done();
+      });
+  });
+
+  test('View issues with one filter', function (done) {
+    chai.request(server)
+      .get('/api/issues/test?open=true')
+      .end((err, res) => {
+        assert.isArray(res.body);
+        done();
+      });
+  });
+
+  test('View issues with multiple filters', function (done) {
+    chai.request(server)
+      .get('/api/issues/test?open=true&created_by=User')
+      .end((err, res) => {
+        assert.isArray(res.body);
+        done();
+      });
+  });
+
+  test('Update one field', function (done) {
+    chai.request(server)
+      .put('/api/issues/test')
+      .send({ _id: testId, issue_text: 'Updated text' })
+      .end((err, res) => {
+        assert.equal(res.body.result, 'successfully updated');
+        done();
+      });
+  });
+
+  test('Update multiple fields', function (done) {
+    chai.request(server)
+      .put('/api/issues/test')
+      .send({ _id: testId, issue_title: 'New title', open: false })
+      .end((err, res) => {
+        assert.equal(res.body.result, 'successfully updated');
+        done();
+      });
+  });
+
+  test('Update missing _id', function (done) {
+    chai.request(server)
+      .put('/api/issues/test')
+      .send({ issue_text: 'Updated text' })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'missing _id');
+        done();
+      });
+  });
+
+  test('Update with no fields', function (done) {
+    chai.request(server)
+      .put('/api/issues/test')
+      .send({ _id: testId })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'no update field(s) sent');
+        done();
+      });
+  });
+
+  test('Update with invalid _id', function (done) {
+    chai.request(server)
+      .put('/api/issues/test')
+      .send({ _id: 'invalidid', issue_text: 'New text' })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'could not update');
+        done();
+      });
+  });
+
+  test('Delete valid _id', function (done) {
+    chai.request(server)
+      .delete('/api/issues/test')
+      .send({ _id: testId })
+      .end((err, res) => {
+        assert.equal(res.body.result, 'successfully deleted');
+        done();
+      });
+  });
+
+  test('Delete invalid _id', function (done) {
+    chai.request(server)
+      .delete('/api/issues/test')
+      .send({ _id: 'invalidid' })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'could not delete');
+        done();
+      });
+  });
+
+  test('Delete missing _id', function (done) {
+    chai.request(server)
+      .delete('/api/issues/test')
+      .send({ })
+      .end((err, res) => {
+        assert.equal(res.body.error, 'missing _id');
+        done();
+      });
+  });
 });
